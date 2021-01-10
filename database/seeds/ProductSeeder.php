18f49a7b2abf38ebfe032f930a4d36d8544635af
Roni Yusuf Manalu <rymanalu@gmail.com>
2021-01-10 14:00:25 +0700

    Create product factory and create user & product seeder

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            factory(Product::class)->create();
+        }
+    }
+}
